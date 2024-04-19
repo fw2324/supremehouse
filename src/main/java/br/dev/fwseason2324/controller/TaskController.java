2bd7e678c8392a07e816c7fcd7475b9f18c99581
gabriel <gabriel.f.silva7@senai.aluno.br>
2024-04-19 09:44:24 -0300

    primeiroenvio

--- src/main/java/br/dev/fwseason2324/controller/TaskController.java
+++ src/main/java/br/dev/fwseason2324/controller/TaskController.java
@@ -1,26 +1,51 @@
 package br.dev.fwseason2324.controller;
 
+
+package br.dev.abrindoportas.WebApp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import br.dev.abrindoportas.WebApp.models.TaskModel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class TaskController {
 
+    List<TaskModel> taskModels = new ArrayList<>();
 
-    @GetMapping("/create)
-    
-
-    public String getCreateString(@RequestParam String param) {
-        return new String();
+    @GetMapping("/create")
+    public String getCreateString(){
+        return "create";
     }
     
+    @PostMapping("/create")
+    public String postCreateString(TaskModel taskModel){
+        // System.out.println("O nome da tarefa é: " + taskModel.getName());
+        Long id = taskModels.size() + 1L;
+        
+        taskModels.add(new TaskModel(id, taskModel.getName(), taskModel.getDate()));
 
-public string getCreateString(){
-    return "create";
-    
+        return "redirect:/list";
+    }
 
-    
+    @GetMapping("/edit/{id}")
+    public ModelAndView getEditString(@PathVariable("id") Long id){
+        
+        ModelAndView mv = new ModelAndView("create");
+
+        TaskModel taskModelFind = taskModels.stream().filter(taskModel -> id.equals(taskModel.getId())).findFirst().get();
+
+        mv.addObject("taskModel", taskModelFind);
+
+        return mv;
+    }
 }
+
